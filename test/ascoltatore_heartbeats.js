f5a6973560a25798619097452caf8f75582ca077
filnik <filnik90@gmail.com>
2013-02-19 15:15:27 +0100

    it WORKS :D

--- test/ascoltatore_heartbeats.js
+++ test/ascoltatore_heartbeats.js
@@ -4,12 +4,30 @@ var _ = require("underscore");
 
 module.exports = function () {
 
-  it("should keep the heartbeats", function (done) {
+    it("should keep the heartbeats", function (done) {
       var that = this;
-      this.instance.on('nodeDeath', done);
+      this.instance.on('nodeDeath', function () {
+          done();
+      });
       this.instance.subscribe('hello', function () {});
-      this.instance.publish("hello", "world", function () {
+      this.instance.publish('hello', "world", function () {
           that.instance.close();
       });
-  });
+    });
+
+    it("should not die before the close", function (done) {
+        var that = this;
+        var youCanDie = false;
+        this.instance.on('nodeDeath', function () {
+            if (youCanDie){
+                done();
+            }
+        });
+        this.instance.subscribe('hello', function () {});
+        this.instance.publish('hello', "world", function () {
+            setTimeout(function delay() {
+                youCanDie = true;
+                that.instance.close();}, 500);
+        });
+    });
 };
--- test/heartbeat_ascoltatore_spec.js
+++ test/heartbeat_ascoltatore_spec.js
@@ -15,5 +15,4 @@ describe(HeartbeatAscoltatore, function() {
     this.instance.close();
   });
 
-
 });
